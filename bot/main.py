7bb78eb6d75be39df4d9f6483dc11a3c0de344f8
Caleb Bliss <calebmbliss@gmail.com>
2025-07-04 18:44:51 -0500

    Edited main.py, added README.md

--- bot/main.py
+++ bot/main.py
@@ -1,13 +1,30 @@
 import discord
-from discord.ext import commands
 import logging
-from dotenv import load_dotenv
 import os
+
+from discord import app_commands
+from discord.ext import commands
+from dotenv import load_dotenv
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
-handler = logging.FileHandler('discord.log', 'utf-8', 'w')
-intents = discord.Intents.default
+intents = discord.Intents.default()
 intents.message_content = True
-intents.members = True
+
+bot = commands.Bot(command_prefix = None, intents = intents)
+
+@bot.event
+async def on_ready():
+    print("Bot is running.")
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} command(s)")
+    except Exception as e:
+        print(e)
+
+@bot.tree.command(name="hello")
+async def hello(interaction: discord.Interaction):
+    print("Hello console!")
+
+bot.run(token)
