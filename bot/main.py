135078db369ea9aa1f984c05f9b5ec6f4b4633f6
Caleb Bliss <calebmbliss@gmail.com>
2025-07-04 19:18:43 -0500

    Added commands folder along with some starter command files.

--- bot/main.py
+++ bot/main.py
@@ -1,10 +1,18 @@
 import discord
 import logging
 import os
+import importlib
 
 from discord import app_commands
 from discord.ext import commands
 from dotenv import load_dotenv
+
+command_files = [
+    "communicate",
+    "startserver",
+    "stopserver",
+    "status"
+]
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
@@ -14,17 +22,20 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix = None, intents = intents)
 
+async def load_commands():
+    for name in command_files:
+        try:
+            modulePath = f"commands.{name}"
+            module = importlib.import_module(modulePath)
+            await module.setup(bot)
+        except Exception as e:
+            print(e)
+
 @bot.event
 async def on_ready():
-    print("Bot is running.")
-    try:
-        synced = await bot.tree.sync()
-        print(f"Synced {len(synced)} command(s)")
-    except Exception as e:
-        print(e)
-
-@bot.tree.command(name="hello")
-async def hello(interaction: discord.Interaction):
-    print("Hello console!")
+    print("Bot is running. Loading and syncing commands..")
+    await load_commands()
+    await bot.tree.sync()
+    print("Synced commands.")
 
 bot.run(token)
