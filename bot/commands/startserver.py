c8fbb47ca0916e1c4a31c905b0913d66449c9691
Caleb Bliss <calebmbliss@gmail.com>
2025-07-04 23:32:31 -0500

    Created config directory, updated .gitignore, and added some config files.

--- bot/commands/startserver.py
+++ bot/commands/startserver.py
@@ -4,7 +4,6 @@ from discord import app_commands, Interaction
 from discord.ext.commands import Bot
 
 
-
 async def setup(bot: Bot):
     def is_admin(interaction: Interaction) -> bool:
         return any(role.name == "Admin" for role in interaction.user.roles)
