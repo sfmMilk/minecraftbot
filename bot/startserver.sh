a01a8c259f0a837e907688b53dd32ee20f7faa98
Caleb Bliss <calebmbliss@gmail.com>
2025-07-05 01:44:23 -0500

    Finished the start/stop bash files (Should work?) and removed comments from startserver.py

--- bot/startserver.sh
+++ bot/startserver.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-echo "[DEBUG] Current dir: $(pwd)"
-echo "[DEBUG] Script dir: $(dirname "$0")"
-echo "[DEBUG] Running as: $(whoami)"
+PARENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+cd $PARENT_DIR/bot # Changes directory to bot. That way I can find config bash lol
+source ./config/config.sh
+cd ./.. # Go up a level outside of bot, this is where your minecraftservers SHOULD be.
+
+if [ -d "./$SERVER_NAME" ]; then
+    cd ./$SERVER_NAME
+    screen -dmS bash -c './start.sh' # ASSUMES start.sh IS REAL!! DANGEROUS!!
+else
+    echo "$SERVER_NAME does not exist!"
+fi
+
+# To the Caleb of whenever he decides to read this. Please make/learn functions in bash. This is ugly. Also it's unfinished.
+
+
--- bot/stopserver.sh
+++ bot/stopserver.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-echo "test!"
+
+screen -S minecraft -X stuff "stop$(printf \\r)"
