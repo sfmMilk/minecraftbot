fd3d2e457658f55f43796431ca51e2b3bf2a37db
Caleb Bliss <calebmbliss@gmail.com>
2025-07-05 00:09:43 -0500

    startserver.py more or less implemented. serverutil.py being worked on.

--- bot/commands/startserver.py
+++ bot/commands/startserver.py
@@ -1,5 +1,6 @@
 import subprocess
 
+from util import serverutil
 from discord import app_commands, Interaction
 from discord.ext.commands import Bot
 
@@ -8,10 +9,16 @@ async def setup(bot: Bot):
     def is_admin(interaction: Interaction) -> bool:
         return any(role.name == "Admin" for role in interaction.user.roles)
     
+    def is_starting() -> bool:
+        return False
+    
     @app_commands.check(is_admin)
     @bot.tree.command(
         name = "startserver",
         description = "Starts the server."
     )
     async def startserver(interaction: Interaction):
+        status = serverutil.getStatus() != None
+        if not status: return
         subprocess.run(["./startserver.sh"])
+        await interaction.response.send_message("Server starting.. Check status with the status command. (Be patient!)")
---
+++ bot/util/serverutil.py
@@ -0,0 +1,11 @@
+from mcstatus import JavaServer
+
+def getStatus():
+    try:
+        server = JavaServer.lookup("bliss")
+        status = server.status()
+        return status
+    except:
+        print("Server unreachable, or an error occured.")
+        return None
+    
